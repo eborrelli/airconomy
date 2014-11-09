777f2c1732a26d3f34727249ad727fcf21d28f53
Elvio Borrelli <elvio@elvio-Satellite-Pro-U400.(none)>
2014-11-10 00:39:00 +0100

    Top 10 arrivals. TODO: check, date, reduce

--- airconomy.py
+++ airconomy.py
@@ -48,7 +48,71 @@ searches_file_num_lines = i+1
 print('Num lines of searches file: ' + str(searches_file_num_lines))
 
 
+# In[1]:
+
+import numpy as np
+
+
+# In[2]:
+
+import pandas as pd
+
+
+# In[4]:
+
+# top ten arrivals in the world in 2013
+numLines = 10000011
+skipLines = 1 # we skip the header line
+numChunks = 64
+chunkSize = np.ceil((numLines - skipLines) / (numChunks / 1.0)).astype(int)
+chunkSize
+
+
 # In[ ]:
 
+reader = pd.read_csv(bookings_file, sep='\s*\^', skiprows=skipLines, chunksize=chunkSize, header=None)
 
+
+# In[ ]:
+
+chunk_results = {}
+for chunk in reader:
+    try:
+        tmp = chunk.ix[:,[12,34]].groupby(['X.13']).sum()
+        for key,value in dict(tmp['X.35']).iteritems():
+            try:
+                chunk_results[key] += value
+            except KeyError:
+                chunk_results[key] = []
+                chunk_results[key].append(value)
+    except Exception as e:
+        pass
+
+
+# In[ ]:
+
+reduced_results = []
+for key,values in chunk_results.iteritems():
+    reduced_results.append((key, sum(values).astype(int)))
+
+
+# In[ ]:
+
+def tuple_sort (a, b):
+    if a[1] < b[1]:
+        return 1
+    elif a[1] > b[1]:
+        return -1
+    else:
+        return cmp(a[0], b[0])
+
+
+# In[ ]:
+
+reduced_results.sort(tuple_sort)
+
+
+# In[ ]:
+
+reduced_results[0:10]
 
